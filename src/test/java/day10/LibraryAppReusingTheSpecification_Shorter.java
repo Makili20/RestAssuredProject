2cbc95125402d05dbb886c9ce1f5060553722cc3
Marijana Igracki <marijana.makili@gmail.com>
2023-03-16 10:07:21 -0400

    Day10:Set Specifiaction at the global level so it can be reusable

---
+++ src/test/java/day10/LibraryAppReusingTheSpecification_Shorter.java
@@ -0,0 +1,72 @@
+package day10;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utilities.ConfigurationReader;
+import utilities.LibraryUtil;
+
+import static io.restassured.RestAssured.*;
+
+public class LibraryAppReusingTheSpecification_Shorter {
+
+
+    @BeforeAll
+    public static void init() {
+
+        RestAssured.baseURI = ConfigurationReader.getProperty("library1.database.url");
+        RestAssured.basePath = "/rest/v1";
+
+        String theToken = LibraryUtil.loginAndGetToken("librarian69@library", "KNPXrm3S");
+        //we have build a resuable request Specification for setting contentType
+        RestAssured.requestSpecification = given().accept(ContentType.JSON)//we waant Json back
+                .log().all()
+                .header("x-library-token", theToken);//we want to set the token header
+
+        RestAssured.responseSpecification = expect().statusCode(200)
+                .contentType(ContentType.JSON)//we are sending Content Type Json
+                .logDetail(LogDetail.ALL);
+
+    }
+
+    @DisplayName("Testing GET/get book categories Endpoint with spec")
+    @Test
+    public void testLibrary() {
+
+        when()
+                .get("/get_book_categories");
+
+
+
+
+    }
+
+    @DisplayName("Testing GET /get_all_users Endpoint with spec")
+    @Test
+    public void testGetAllUsers() {
+
+
+        when()
+                .get(" /get_all_users");
+
+
+    }
+
+    @DisplayName("Testing GET /dashboard_stats Endpoint with spec")
+    @Test
+    public void testGet_Dashboard_stats() {
+
+        when()
+                .get(" /dashboard_stats");
+
+
+    }
+}
+
+
+
+
+
